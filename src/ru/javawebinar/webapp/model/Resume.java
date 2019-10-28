a98da26c4bb57db62c6f7eff3f6c9deffbaa5d72
DimonFX <DimonFX@yandex.ru>
2019-10-28 23:28:37 +0300

    1 commit

---
+++ src/ru/javawebinar/webapp/model/Resume.java
@@ -0,0 +1,10 @@
+package ru.javawebinar.webapp.model;
+
+import java.util.Collection;
+
+public class Resume {
+    String fullName;
+    String [] some;
+    Collection <String> collection;
+    Collection <String> collection3;
+}
